be79bda28a98526a4f1172d4cf4f8a570b2eecd6
Simon Vreman <info@simonvreman.nl>
2023-12-06 17:41:52 +0100

    Add proper title to meals tab

--- Vreetmeter/Tabs/MealsTab.swift
+++ Vreetmeter/Tabs/MealsTab.swift
@@ -3,6 +3,8 @@ import SwiftUI
 
 struct MealsTab: View {
     var body: some View {
-        Text("Meals")
+        NavigationView {
+            VStack { }.navigationTitle("Meals")
+        }
     }
 }
