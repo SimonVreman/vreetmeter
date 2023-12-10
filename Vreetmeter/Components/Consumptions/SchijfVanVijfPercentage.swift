c87c806626e4397865b509b770015b667a0da8ef
Simon Vreman <info@simonvreman.nl>
2023-12-10 11:00:36 +0100

    Implement SVV percentage component

---
+++ Vreetmeter/Components/Consumptions/SchijfVanVijfPercentage.swift
@@ -0,0 +1,18 @@
+//
+//  SchijfVanVijfPercentage.swift
+//  Vreetmeter
+//
+//  Created by Simon2 on 10/12/2023.
+//
+
+import SwiftUI
+
+struct SchijfVanVijfPercentage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SchijfVanVijfPercentage()
+}
