a67936f8fd2080f060d250a65ed87893191ec88a
Simon Vreman <info@simonvreman.nl>
2023-12-10 10:44:52 +0100

    Add schijf van vijf type definitions

---
+++ Vreetmeter/Data/Nutritional/SchijfVanVijf.swift
@@ -0,0 +1,58 @@
+
+import Foundation
+
+/**
+ These definitions are based on the "Richtlijnen Schijf van Vijf (2020)" document from Voedingscentrum
+ */
+
+protocol SchijfVanVijf {
+    var isSchijfVanVijf: Bool { get }
+    var schijfVanVijfCategory: SchijfVanVijfCategory? { get }
+    var schijfVanVijfColumn: SchijfVanVijfColumn? { get }
+}
+
+enum SchijfVanVijfCategory: Int {
+    case vegetablesAndFruits = 1
+    case oilsAndFats = 2
+    case fishMeatDairyAndNuts = 3
+    case breadGrainsAndPotatos = 4
+    case waterTeaAndCoffee = 5
+    
+    init(column: SchijfVanVijfColumn) {
+        switch column {
+        case .vegetables:
+            fallthrough
+        case .fruits:
+            self = .vegetablesAndFruits
+        case .fats:
+            self = .oilsAndFats
+        case .fishAndMeat:
+            fallthrough
+        case .nuts:
+            fallthrough
+        case .dairy:
+            fallthrough
+        case .cheese:
+            self = .fishMeatDairyAndNuts
+        case .bread:
+            fallthrough
+        case .grainAndPotatos:
+            self = .breadGrainsAndPotatos
+        case .drinks:
+            self = .waterTeaAndCoffee
+        }
+    }
+}
+
+enum SchijfVanVijfColumn: Int {
+    case vegetables = 1
+    case fruits = 2
+    case fats = 3
+    case fishAndMeat = 4
+    case nuts = 5
+    case dairy = 6
+    case cheese = 7
+    case bread = 8
+    case grainAndPotatos = 9
+    case drinks = 10
+}
