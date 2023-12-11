70668101e5e7b9afefd8a0a0e13a47ffac6fdbd1
Simon Vreman <info@simonvreman.nl>
2023-12-11 19:18:42 +0100

    Add conditional showing to weight chart

--- Vreetmeter/Components/Health/WeightCard.swift
+++ Vreetmeter/Components/Health/WeightCard.swift
@@ -5,21 +5,30 @@ import Charts
 struct WeightCard: View {
     var data: [NumericalDatePoint]
     
+    func getAverage(days: Int) -> Double? {
+        let start = Calendar.current.date(byAdding: .day, value: -7, to: Date.now)!
+        let recentPoints = data.filter { $0.date > start }
+        if recentPoints.isEmpty { return nil }
+        return recentPoints.reduce(0) { $0 + $1.value } / Double(recentPoints.count)
+    }
+    
     var body: some View {
         GroupBox {
             VStack(alignment: .leading) {
-                HStack(alignment: .bottom) {
-                    let mostRecent = self.data.last?.value
-                    Text(mostRecent != nil ? "\(mostRecent!, specifier: "%.1f")" : "--.-")
+                HStack(alignment: .firstTextBaseline) {
+                    let average = getAverage(days: 7)
+                    
+                    Text(average != nil ? "\(average!, specifier: "%.1f")" : "--.-")
                         .font(.largeTitle).fontDesign(.rounded).fontWeight(.semibold)
                     + Text("kg").font(.headline).foregroundStyle(.primary.secondary)
                     
-                    Text("last 30 days").font(.headline).foregroundStyle(.primary.secondary)
+                    Spacer()
+                    
+                    Text("7 day average").font(.headline).foregroundStyle(.primary.secondary)
                 }
                 
-                WeightChart(data: data)
+                if data.count > 1 { WeightChart(data: data).frame(height: 200) }
             }
         }.cardBackgroundAndShadow()
-            .frame(height: 300)
     }
 }
