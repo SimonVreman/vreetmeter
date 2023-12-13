642d75a50f66060985ab32b6e710df33d42e8957
Simon Vreman <info@simonvreman.nl>
2023-12-13 19:51:09 +0100

    Add nutritional targets

---
+++ Vreetmeter/Data/Nutritional/NutritionalTargets.swift
@@ -0,0 +1,21 @@
+
+import Foundation
+
+/**
+ These definitions are based on data from Voedingcentrum
+ */
+
+struct NutritionalTargets {
+    let columnTargets: [SchijfVanVijfColumn:(min: Int, max: Int)] = [
+        .vegetables: (min: 250, max: 250),
+        .fruits: (min: 200, max: 200),
+        .fats: (min: 65, max: 65),
+        .fishAndMeat: (min: 100, max: 100),
+        .nuts: (min: 25, max: 25),
+        .dairy: (min: 300, max: 450),
+        .cheese: (min: 40, max: 40),
+        .bread: (min: 210, max: 280),
+        .grainAndPotatos: (min: 200, max: 350),
+        .drinks: (min: 1500, max: 2000)
+    ]
+}
