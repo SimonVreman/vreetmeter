c9f5255160f2ded5c377eb2e34b91efa06238d5c
Simon Vreman <info@simonvreman.nl>
2024-02-24 17:23:02 +0100

    Add workout plan list

--- Vreetmeter/Views/Workout/WorkoutPlanView.swift
+++ Vreetmeter/Views/Workout/WorkoutPlanView.swift
@@ -1,10 +1,33 @@
 
 import SwiftUI
 
+private struct WorkoutDay: Identifiable {
+    var id: UUID
+    var template: WorkoutTemplate?
+}
+
 struct WorkoutPlanView: View {
     var plan: WorkoutPlan
     
+    private var days: [WorkoutDay] {
+        var days: [WorkoutDay] = []
+        for i in 0...(plan.workouts.count + plan.restDays.count) {
+            if (plan.restDays.contains(i)) { days.append(WorkoutDay(id: UUID())) }
+            let workout = plan.workouts[plan.restDays.filter { $0 < i }.count]
+            days.append(WorkoutDay(id: workout.id, template: workout))
+        }
+        return days
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(days) { day in
+                if day.template == nil {
+                    Text("Rest day")
+                } else {
+                    NavigationLink(day.template!.name, value: day.template!)
+                }
+            }
+        }
     }
 }
